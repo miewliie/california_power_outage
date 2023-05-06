096079401aec2b68f633a7d9f61566cda2727ee6
Miewliie <miewliie@gmail.com>
2023-05-05 17:03:21 -0700

    [core] refactor request api into core directory

---
+++ california_power_outage/core/arcgis_api.py
@@ -0,0 +1,12 @@
+import requests
+
+URL = 'https://services.arcgis.com/BLN4oKB0N1YSgvY8/arcgis/rest/services/Power_Outages_(' \
+      'View)/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json '
+
+
+def request_api(url: str = URL):
+    response = requests.get(url)
+    return response.json()
+
+
+
